7675be2fce272e791d3723430ba76a9b5816237b
Batipton <bryson.tipton@gmail.com>
2024-07-09 21:26:39 -0400

    Update page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,12 +12,10 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* @ts-expect-error Server Component */
-        <CardWrapper />}
+        <CardWrapper />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* @ts-expect-error Server Component */
-        <LatestPosts />}
+        <LatestPosts />
       </div>
       
     </main>
