5d64219884b798ad9e8dbcff4502441fcd404cac
Batipton <bryson.tipton@gmail.com>
2024-07-09 21:29:07 -0400

    Update page.tsx

--- app/dashboard/friends/[id]/page.tsx
+++ app/dashboard/friends/[id]/page.tsx
@@ -40,7 +40,6 @@ export default async function Page({ params }: { params: { id: string } }) {
         </div>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-          {/* @ts-expect-error Server Component */}
           <LatestPosts />
       </div>
     </div>
