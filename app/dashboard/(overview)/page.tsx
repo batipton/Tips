d9f69d12b96ecbffcfac6fde20d3d7850f76d513
Batipton <bryson.tipton@gmail.com>
2024-07-09 21:16:17 -0400

    Moving Directives

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,12 +12,12 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* @ts-expect-error Server Component */}
-        <CardWrapper />
+        {/* @ts-expect-error Server Component */
+        <CardWrapper />}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* @ts-expect-error Server Component */}
-        <LatestPosts />
+        {/* @ts-expect-error Server Component */
+        <LatestPosts />}
       </div>
       
     </main>
