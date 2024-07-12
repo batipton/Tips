b1e08b93f0ecea625738e23e2d83ec33a9e2154d
Batipton <bryson.tipton@gmail.com>
2024-07-12 13:12:39 -0400

    Taking out expect errors

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -32,7 +32,6 @@ export default async function Page() {
             </div>
           </div>
           <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-              {/* @ts-expect-error Async Server Component */}
               <LatestPosts />
           </div>
         </div>
