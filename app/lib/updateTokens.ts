a183fd095df29f5b4fd41d0e038a83b5b894f76e
Batipton <bryson.tipton@gmail.com>
2024-07-12 12:58:50 -0400

    Update updateTokens.ts

--- app/lib/updateTokens.ts
+++ app/lib/updateTokens.ts
@@ -3,3 +3,7 @@
 import { sql } from '@vercel/postgres';
 
 await sql`UPDATE users SET tokens = tokens + 100`;
+
+export async function GET() {
+    await sql`UPDATE users SET tokens = tokens + 100`;
+  }
