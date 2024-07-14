a249c58c70c4a44a25de0ed5e88bf31005db6534
Batipton <bryson.tipton@gmail.com>
2024-07-14 10:06:56 -0400

    changed cron job

--- app/lib/updateTokens.ts
+++ app/lib/updateTokens.ts
@@ -2,7 +2,6 @@
 
 import { sql } from '@vercel/postgres';
 
-await sql`UPDATE users SET tokens = tokens + 100`;
 
 export async function GET() {
     await sql`UPDATE users SET tokens = tokens + 100`;
