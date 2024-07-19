8677d3934323229139b5245d6bb42d28f1c558f0
Batipton <bryson.tipton@gmail.com>
2024-07-19 13:51:12 -0400

    Changed Cron Job time

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,10 +3,7 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+    <div>
       <p className="text-[44px]">Tips</p>
     </div>
   );
