59e5e733d1b7b9f3e50dfd8ecbbecf4495101491
Batipton <bryson.tipton@gmail.com>
2024-07-29 12:07:28 -0400

    Update follower-information.tsx

--- app/ui/followers/follower-information.tsx
+++ app/ui/followers/follower-information.tsx
@@ -18,7 +18,7 @@ const style = {
   p: 4,
 };
 
-export default function FollowerModal({followers=[], followerCount=0}) {
+export default function FollowerModal({followers, followerCount}:{followers:QueryResultRow[], followerCount:number}) {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
