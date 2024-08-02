1fdb2080dc349d213abf36cdd15bbeec8f588f32
Batipton <bryson.tipton@gmail.com>
2024-08-02 01:14:43 -0400

    Update post-settings.tsx

--- app/ui/posts/post-settings.tsx
+++ app/ui/posts/post-settings.tsx
@@ -5,6 +5,7 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { deletePost } from '@/app/lib/actions';
+import { redirect } from 'next/navigation';
 
 const style = {
     position: 'absolute' as 'absolute',
