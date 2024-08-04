0566496a311c85c197b5c50be8255028d0a61018
Batipton <bryson.tipton@gmail.com>
2024-08-04 11:26:46 -0400

    Refactored Post

--- app/ui/posts/latest-posts.tsx
+++ app/ui/posts/latest-posts.tsx
@@ -43,54 +43,7 @@ export default async function LatestPosts({ mode, id }:{ mode:string, id:string 
           {/* NOTE: comment in this code when you get to this point in the course */}
           <div className="bg-white px-6">
             {latestPosts.map(async (post, i) => {
-              return <Post post={post} userid={userid} key={i} tokens={user!.tokens} />
-              // console.log(post);
-              // const time = timeAgo.format(new Date(post.date))
-              // const htmlToReactParser = new HtmlToReactParser();
-              // const reactElement = htmlToReactParser.parse(post.text);
-              // const comments = await fetchComments(post.id);
-              // return (
-              //   <div key={post.id} >
-              //     <div
-              //       className={clsx(
-              //         'flex flex-row items-center justify-between py-4',
-              //         {
-              //           'border-t': i !== 0,
-              //         },
-              //       )}
-              //     >
-              //       <div className="flex items-center">
-              //         <img
-              //           src={post.image_url}
-              //           alt={`${post.username}'s profile picture`}
-              //           className="mr-4 h-10 w-10 rounded-full"
-
-              //         />
-              //         <div className="min-w-0">
-              //           <p>
-              //             <Link href={`/home/followers/${post.customer_id}`} className="truncate text-sm font-semibold md:text-base hover:underline">
-              //               {post.name} 
-              //             </Link>
-              //             <Link href={`/home/followers/${post.customer_id}`} className="truncate text-sm md:text-base">
-              //               {` $${post.username}`}
-              //             </Link>
-              //           </p>
-              //           <p className="hidden text-sm text-gray-500 sm:block">
-              //             {time}
-              //           </p>
-              //         </div>
-              //       </div>
-              //       <PostSettings userid={userid} posterid={post.customer_id} postid={post.id} />
-              //     </div>
-              //     <div>
-              //       {reactElement}
-              //     </div>
-              //     <div className="flex">
-              //       <LikeButton id={post.id} tips={post.tips} userid={session.user?.id} posterid={post.customer_id} tokens={user!.tokens}/>
-              //       <CommentButton comments={comments} postid={post.id} userid={userid} posterid={post.customer_id} />    
-              //     </div>
-              //   </div>
-              // );
+              return <Post id={post.id} />        
             })}
           </div>
         </div>
