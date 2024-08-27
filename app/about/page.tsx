a9dfcb35f8985c7303bfdd8d3fae28a0e9feb30f
Batipton <bryson.tipton@gmail.com>
2024-08-26 20:31:40 -0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,9 +20,9 @@ export default function() {
             <h2 className="text-3xl font-semibold mb-4">How It Works</h2>
             <p className="text-lg">
               Every day, each user can redeem 15 tokens that they can use to tip posts they find valuable or enjoyable.
-              A user is not limited to giving a post 1 token, they may give it 2, 3 or even 15 tokens. Users may earn
-              even more tokens by making posts.  Tipping allows users to directly support creators and contribute to a vibrant and rewarding community experience.
-              For every 2 tokens a post gets, the user who posted it will earn 1 token.
+              A user is not limited to giving a post 1 token, they may give it 2, 3 or even 15 tokens.  
+              Tipping allows users to creating A rewarding community experience.
+              For every 2 tokens a post gets, the user who posted it will earn 1 token. 
             </p>
           </section>
 
