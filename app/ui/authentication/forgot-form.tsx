6868cc0565933591fc64b05bc9f32e831beca5c1
Batipton <bryson.tipton@gmail.com>
2024-08-28 21:02:50 -0400

    Update forgot-form.tsx

--- app/ui/authentication/forgot-form.tsx
+++ app/ui/authentication/forgot-form.tsx
@@ -76,7 +76,6 @@ export default function ForgotForm() {
                         name="email"
                         placeholder="Enter your email address"
                         required
-                        {...register('email')}
                     />
                     <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                     </div>
