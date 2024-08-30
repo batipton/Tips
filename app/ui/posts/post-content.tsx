ebc4e70cac29d4b462217f91974de4115c30380a
Batipton <bryson.tipton@gmail.com>
2024-08-30 12:45:55 -0400

    Update post-content.tsx

--- app/ui/posts/post-content.tsx
+++ app/ui/posts/post-content.tsx
@@ -64,7 +64,7 @@ export default async function Post({id}:{id:string}) {
                 {reactElement}
               </div>
               <div className="flex">
-                <LikeButton initialTips={post.tips} id={post.id} userid={session.user?.id} posterid={post.customer_id} />
+                <LikeButton initialTips={post.tips} id={post.id!} userid={session.user?.id!} posterid={post.customer_id!} />
                 <CommentButton comments={comments} postid={post.id} userid={userid} posterid={post.customer_id} />    
               </div>
             </div>
